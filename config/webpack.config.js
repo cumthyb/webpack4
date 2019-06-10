b5d684325d7c2f987d1d9647df2d2ad5af7e0485
hongyongbo <hongyongbo@krspace.cn>
2019-06-10 14:44:21 +0800

    feat: 图片、字体、样式打包

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
@@ -15,23 +16,51 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpg|jpeg|bmp|ico|svg)$/i,
+        test: /\.(png|jpg|jpeg|bmp|ico)$/i,
+        include: [
+          path.resolve(__dirname, '../static/imgs')
+        ],
         use: {
           loader: 'url-loader',
           options: {
             limit: 20480,//20k
             name: '[name][hash].[ext]',
-            outputPath: 'static/imgs'
+            outputPath: 'static/imgs'//拷贝至打包结果下的static/imgs路径下
           }
         },
       },
       {
-        test: /\.css/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.(eot|ttf|woff|svg|js)$/i,
+        include: [
+          path.resolve(__dirname, '../static/font') //src目录下可能有svg资源，在此特别设置inclule为了只打包与字体图标相关的svg资源
+        ],
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name][hash].[ext]',
+            outputPath: 'static/fonts',// 输出到打包目录下的static/fonts路径，保持资源间的相对位置不变，也便于管理
+          }
+        },
       },
       {
-        test: /\.less/i,
-        use: ['style-loader', 'css-loader', 'less-loader']
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          "postcss-loader",
+          'less-loader'
+        ]
       },
     ],
   },
@@ -40,6 +69,17 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html'
-    })
-  ]
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  resolve: {
+    alias: {
+      'static': path.join(__dirname, '..', 'static'),
+    }
+  }
 }
