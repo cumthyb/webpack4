18666f5aab56f5a419b926e241e9890844fa6e5b
cumthyb <cumthyb@163.com>
2019-07-21 11:09:36 +0800

    feat:add uglifyjs config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,10 +1,26 @@
-const path=require('path')
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports={
-  mode:'development',// 设置开发模式下
-  entry:'./src/index.js', //指定入口文件 注意路径是相对与根目录的
-  output:{
-    filename:'bundle.js', //指定生成的文件名
-    path:path.resolve(__dirname,'../dist') // 指定生成chunk的存放位置，注意路径必须四绝对路径
-  }
+module.exports = {
+  mode: 'production',// 设置开发模式下
+  entry: './src/index.js', //指定入口文件 注意路径是相对与根目录的
+  output: {
+    filename: 'bundle.js', //指定生成的文件名
+    path: path.resolve(__dirname, '../dist') // 指定生成chunk的存放位置，注意路径必须四绝对路径
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          // warnings: false,
+          drop_console: true,
+          drop_debugger: false,
+          pure_funcs: ['console.log']//移除console
+        }
+      },
+      sourceMap: false,
+      parallel: true
+    }),
+  ]
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Header from './header.js'
 import Sidebar from './sidebar.js'
 import Content from './content.js'
 
+debugger
+console.log(121213);
+
 new Header()
 new Sidebar()
 new Content()
